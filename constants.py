e6d21633538f2b3ffcf219d11b8293768349b8ac
Avraam Lazarev <39047410+Avraam305@users.noreply.github.com>
2021-01-26 15:24:22 +0200

    Update constants.py
    constants

--- constants.py
+++ constants.py
@@ -1,5 +1,4 @@
-yelp_key = 'APgDr0LIe8qZsZ-H5iNu2Z4KtCLIc0aH5hRldvup03qN_Qf08XH2OaWEN1inKeTz0pqj7Rg_p9Pt6mnFQuFP8E2j4u7cy7-p-' \
-      '1CBBvx4Bvs4ojpsd3NrbuVg6YIJYHYx'
+yelp_key = 'APgDr0LIe8qZsZ-H5iNu2Z4KtCLIc0aH5hRldvup03qN_Qf08XH2OaWEN1inKeTz0pqj7Rg_p9Pt6mnFQuFP8E2j4u7cy7-p-1CBBvx4Bvs4ojpsd3NrbuVg6YIJYHYx'
 google_key = 'AIzaSyCbjehXKBNFEdyF2tgwWewd06F6ONawRt0'
 url_yelp = 'https://api.yelp.com/v3/businesses/search'
 db_password = '12345'
