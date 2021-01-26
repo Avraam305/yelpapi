5a1d757e0b9ee6d549c3f801e6c47583feb2f48a
Avraam Lazarev <39047410+Avraam305@users.noreply.github.com>
2021-01-26 15:08:42 +0200

    Create constants.py
    constants

---
+++ constants.py
@@ -0,0 +1,8 @@
+yelp_key = 'APgDr0LIe8qZsZ-H5iNu2Z4KtCLIc0aH5hRldvup03qN_Qf08XH2OaWEN1inKeTz0pqj7Rg_p9Pt6mnFQuFP8E2j4u7cy7-p-' \
+      '1CBBvx4Bvs4ojpsd3NrbuVg6YIJYHYx'
+google_key = 'AIzaSyCbjehXKBNFEdyF2tgwWewd06F6ONawRt0'
+url_yelp = 'https://api.yelp.com/v3/businesses/search'
+db_password = '12345'
+db_user = 'root'
+db_host = 'localhost'
+db_name = 'yelpbusinessinfo'
